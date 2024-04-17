7daba9c60bf981b62ad1094b8a7c461de3ce7c4f
Neerav Panchal <neeravpanchal26@gmail.com>
2024-04-17 00:56:20 +0000

    Step 5: Accept User Input

--- Geometry.Console/Program.cs
+++ Geometry.Console/Program.cs
@@ -21,35 +21,62 @@ services.AddFeatureManagement(configuration);
 var serviceProvider = services.BuildServiceProvider();
 
 var featureManager = serviceProvider.GetRequiredService<IFeatureManager>();
-if (await featureManager.IsEnabledAsync("Square"))
-{
-    // Provide access to Square
-    var square = new Square(5);
-    Console.WriteLine("Square - Area: " + square.CalculateArea() + ", Perimeter: " + square.CalculatePerimeter());
-}
-else
-{
-    Console.WriteLine("Square feature is disabled.");
-}
+Console.WriteLine("Choose a shape to calculate (1: Square, 2: Rectangle, 3: Triangle):");
+int choice = int.Parse(Console.ReadLine());
 
-if (await featureManager.IsEnabledAsync("Rectangle"))
+switch (choice)
 {
-    // Provide access to Rectangle
-    var rectangle = new Rectangle(4, 6);
-    Console.WriteLine("Rectangle - Area: " + rectangle.CalculateArea() + ", Perimeter: " + rectangle.CalculatePerimeter());
-}
-else
-{
-    Console.WriteLine("Rectangle feature is disabled.");
-}
+    case 1: // Square
+        Console.WriteLine("Enter side length for the Square:");
+        double squareSideLength = double.Parse(Console.ReadLine());
+        if (await featureManager.IsEnabledAsync("Square"))
+        {
+            // Provide access to Square
+            var square = new Square(squareSideLength);
+            Console.WriteLine("Square - Area: " + square.CalculateArea() + ", Perimeter: " + square.CalculatePerimeter());
+        }
+        else
+        {
+            Console.WriteLine("Square feature is disabled.");
+        }
+        break;
 
-if (await featureManager.IsEnabledAsync("Triangle"))
-{
-    // Provide access to Triangle
-    var triangle = new Triangle(3, 4, 5);
-    Console.WriteLine("Triangle - Area: " + triangle.CalculateArea() + ", Perimeter: " + triangle.CalculatePerimeter());
-}
-else
-{
-    Console.WriteLine("Triangle feature is disabled.");
+    case 2: // Rectangle
+        Console.WriteLine("Enter length and width for the Rectangle (separated by space):");
+        string[] rectangleDimensions = Console.ReadLine().Split(' ');
+        double rectangleLength = double.Parse(rectangleDimensions[0]);
+        double rectangleWidth = double.Parse(rectangleDimensions[1]);
+        if (await featureManager.IsEnabledAsync("Rectangle"))
+        {
+            // Provide access to Rectangle
+            var rectangle = new Rectangle(rectangleLength, rectangleWidth);
+            Console.WriteLine("Rectangle - Area: " + rectangle.CalculateArea() + ", Perimeter: " + rectangle.CalculatePerimeter());
+        }
+        else
+        {
+            Console.WriteLine("Rectangle feature is disabled.");
+        }
+        break;
+
+    case 3: // Triangle
+        Console.WriteLine("Enter three side lengths for the Triangle (separated by space):");
+        string[] triangleDimensions = Console.ReadLine().Split(' ');
+        double triangleSideA = double.Parse(triangleDimensions[0]);
+        double triangleSideB = double.Parse(triangleDimensions[1]);
+        double triangleSideC = double.Parse(triangleDimensions[2]);
+        if (await featureManager.IsEnabledAsync("Triangle"))
+        {
+            // Provide access to Triangle
+            var triangle = new Triangle(triangleSideA, triangleSideB, triangleSideC);
+            Console.WriteLine("Triangle - Area: " + triangle.CalculateArea() + ", Perimeter: " + triangle.CalculatePerimeter());
+        }
+        else
+        {
+            Console.WriteLine("Triangle feature is disabled.");
+        }
+        break;
+
+    default:
+        Console.WriteLine("Invalid choice. Please choose a number between 1 and 3.");
+        break;
 }

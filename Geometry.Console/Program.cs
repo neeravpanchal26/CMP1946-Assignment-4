3d33810666e6f4c154632998d2d766776fab8322
Neerav Panchal <neeravpanchal26@gmail.com>
2024-04-17 00:22:17 +0000

    Step 2: Setup Feature Management

--- Geometry.Console/Program.cs
+++ Geometry.Console/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+var featureManagement = new Dictionary<string, string> {{ "FeatureManagement:Square", "true"}, { "FeatureManagement:Rectangle", "false"}, { "FeatureManagement:Triangle", "true"}};
+
+IConfigurationRoot configuration = new ConfigurationBuilder().AddInMemoryCollection(featureManagement).Build();

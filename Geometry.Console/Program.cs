86517525c424dad4d3a5b55b8b60756645c39087
Neerav Panchal <neeravpanchal26@gmail.com>
2024-04-17 00:47:23 +0000

    Step 4: Control Access to Shapes

--- Geometry.Console/Program.cs
+++ Geometry.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.FeatureManagement;
 using Microsoft.Extensions.Configuration;
+using Geometry.Library;
 
 // Your configuration setup
 var featureManagement = new Dictionary<string, string> 
@@ -18,3 +19,37 @@ IConfigurationRoot configuration = new ConfigurationBuilder()
 var services = new ServiceCollection();
 services.AddFeatureManagement(configuration);
 var serviceProvider = services.BuildServiceProvider();
+
+var featureManager = serviceProvider.GetRequiredService<IFeatureManager>();
+if (await featureManager.IsEnabledAsync("Square"))
+{
+    // Provide access to Square
+    var square = new Square(5);
+    Console.WriteLine("Square - Area: " + square.CalculateArea() + ", Perimeter: " + square.CalculatePerimeter());
+}
+else
+{
+    Console.WriteLine("Square feature is disabled.");
+}
+
+if (await featureManager.IsEnabledAsync("Rectangle"))
+{
+    // Provide access to Rectangle
+    var rectangle = new Rectangle(4, 6);
+    Console.WriteLine("Rectangle - Area: " + rectangle.CalculateArea() + ", Perimeter: " + rectangle.CalculatePerimeter());
+}
+else
+{
+    Console.WriteLine("Rectangle feature is disabled.");
+}
+
+if (await featureManager.IsEnabledAsync("Triangle"))
+{
+    // Provide access to Triangle
+    var triangle = new Triangle(3, 4, 5);
+    Console.WriteLine("Triangle - Area: " + triangle.CalculateArea() + ", Perimeter: " + triangle.CalculatePerimeter());
+}
+else
+{
+    Console.WriteLine("Triangle feature is disabled.");
+}
